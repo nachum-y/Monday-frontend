e499aa2584ba9e84b5fced53c81b79f95d35ea65
Nachum Yosef <nachumy7@gmail.com>
2022-07-20 20:52:23 +0300

    mm

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// import store from './store'
 import './styles/styles.scss'
 
 const app = createApp(App)
 
+
 app.use(router)
-app.use(store)
+// app.use(store)
 
 app.mount('#app')
