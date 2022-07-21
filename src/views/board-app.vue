ed22897bac4cb12ced8cc1b1a38aab81f7bfa473
Nachum Yosef <nachumy7@gmail.com>
2022-07-21 19:17:02 +0300

    commit

--- src/views/board-app.vue
+++ src/views/board-app.vue
@@ -1,9 +1,11 @@
 <template >
-    <board-header/>
-    <group-list />
+    <board-header />
+    <!-- <group-list /> -->
+    <group-list2 />
 </template>
 <script>
 import groupList from '../components/board/group-list.cmp.vue'
+import groupList2 from '../components/board/group-list-copy.cmp.vue'
 import appHeader from '../components/app-header/app-header.cmp.vue'
 import boardHeader from '../components/board/board-header/board-header.cmp.vue'
 export default {
@@ -14,10 +16,11 @@ export default {
         }
     },
     components: {
-    groupList,
-    appHeader,
-    boardHeader,
-}
+        groupList,
+        appHeader,
+        boardHeader,
+        groupList2
+    }
 }
 </script>
 <style >
