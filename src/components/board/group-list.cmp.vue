d999ba52d8cf3aaa00ea2a7e504e3e3f7c8469d2
Nachum Yosef <nachumy7@gmail.com>
2022-07-21 19:50:41 +0300

    new vv

--- src/components/board/group-list.cmp.vue
+++ src/components/board/group-list.cmp.vue
@@ -1,6 +1,5 @@
 <template>
     <div v-if="board" class="board-content">
-        <!-- <priority /> -->
         <div class="board-content-group" v-for="group in board.groups" :key="group.id">
             <div class="group-title">
                 <span ref="groupTitle" contenteditable="true"
@@ -32,7 +31,7 @@
                     <span>{{ col.title }}</span>
                 </div>
             </div>
-            <div class="board-content-group-row" v-for="task in group.tasks" :key="task.id">
+            <div class="board-content-group-row" v-for="(task, idx) in group.tasks" :key="task.id">
                 <div class="col fixed">
                     <div class="task-item">
                         <div class="row-menu">
@@ -47,7 +46,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col" v-for="(col) in board.colsOrder.slice(1)" :key="col.type">
+                <div class="col" v-for="(col, idx) in board.colsOrder.slice(1)" :key="col.type">
                     <component :is="col.type" :task="taskForDisplay(task.cols, col.type)">
                     </component>
                 </div>
@@ -80,6 +79,7 @@
                     d="M10.75 3C10.75 2.58579 10.4142 2.25 10 2.25C9.58579 2.25 9.25 2.58579 9.25 3V9.25H3C2.58579 9.25 2.25 9.58579 2.25 10C2.25 10.4142 2.58579 10.75 3 10.75H9.25V17C9.25 17.4142 9.58579 17.75 10 17.75C10.4142 17.75 10.75 17.4142 10.75 17V10.75H17C17.4142 10.75 17.75 10.4142 17.75 10C17.75 9.58579 17.4142 9.25 17 9.25H10.75V3Z"
                     fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
             </svg>Add new group </button>
+
     </div>
 
 
@@ -96,13 +96,13 @@
 import { boardService } from '../../services/board-service.js'
 import date from './board-col/date.cmp.vue'
 import creationLog from './board-col/creationLog.cmp.vue'
-import label from './board-col/label.cmp.vue'
+import labelCmp from './board-col/label.cmp.vue'
 import lastUpdated from './board-col/last-updated.cmp.vue'
 import location from './board-col/location.cmp.vue'
 import person from './board-col/person.cmp.vue'
 import priority from './board-col/priority.cmp.vue'
 import status from './board-col/status.cmp.vue'
-import text from './board-col/text.cmp.vue'
+import textCmp from './board-col/text.cmp.vue'
 import timeline from './board-col/timeline.cmp.vue'
 
 export default {
@@ -118,17 +118,16 @@ export default {
         this.board = this.$store.getters.board
 
     },
-    
     components: {
         creationLog,
         date,
-        label,
+        labelCmp,
         lastUpdated,
         location,
         person,
         priority,
         status,
-        text,
+        textCmp,
         timeline,
     },
     methods: {
@@ -156,9 +155,6 @@ export default {
             return col
 
         }
-    },
-    components: {
-        // draggable,
     },
     computed: {
         draggingInfo() {
