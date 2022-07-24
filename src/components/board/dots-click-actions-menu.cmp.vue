625ad6e71bdb53dbcf225bd8ddb05303d425b258
Nachum Yosef <nachumy7@gmail.com>
2022-07-24 12:26:54 +0300

    add css to action modal

--- src/components/board/dots-click-actions-menu.cmp.vue
+++ src/components/board/dots-click-actions-menu.cmp.vue
@@ -5,6 +5,31 @@
 
             </i>
         </div>
+        <div class="action-menu">Delete
+            <i class="icon-v2-delete-line">
+
+            </i>
+        </div>
+        <div class="action-menu">Delete
+            <i class="icon-v2-delete-line">
+
+            </i>
+        </div>
+        <div class="action-menu">Delete
+            <i class="icon-v2-delete-line">
+
+            </i>
+        </div>
+        <div class="action-menu">Delete
+            <i class="icon-v2-delete-line">
+
+            </i>
+        </div>
+        <div class="action-menu">Delete
+            <i class="icon-v2-delete-line">
+
+            </i>
+        </div>
 
     </section>
 </template>
@@ -36,7 +61,7 @@ export default {
             const top = this.pos.eltop
             const left = this.pos.left
             if (this.pos.left + 300 > x) return `top:${top}px; left:${this.pos.left - 300}px;`
-            return `top:${top+10}px; left:${this.pos.left-10}px;`
+            return `top:${top + 10}px; left:${this.pos.left - 10}px;`
         }
     },
     created() {
@@ -55,7 +80,7 @@ export default {
     width: 18rem;
     font-size: 0.875rem;
     position: absolute;
-    z-index: 10000000000;
+    z-index: 1000 !important;
     background-color: #fff;
     box-shadow: 0px 8px 10px 1px rgb(0 0 0 / 14%), 0px 3px 14px 2px rgb(0 0 0 / 12%), 0px 5px 5px -3px rgb(0 0 0 / 20%);
 }
