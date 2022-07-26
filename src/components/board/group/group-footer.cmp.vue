4895a7363b7c7547b991789545ce4002c1f438dc
Meital Twito <tw.meital@gmail.com>
2022-07-26 10:09:19 +0300

    footer left corner

--- src/components/board/group/group-footer.cmp.vue
+++ src/components/board/group/group-footer.cmp.vue
@@ -5,9 +5,10 @@
                 <div class="row-menu">
 
                 </div>
+            <div class="board-content-group-row-footer-corner"></div>
+            </div>
             </div>
 
-        </div>
         <div class="footer-col" v-for="col in colsOrder.slice(1)" :key="col.type"></div>
     </div>
 </template>
@@ -41,4 +42,18 @@ export default {
 }
 </script>
 <style>
+    .board-content-group-row-footer-corner{
+        width: 8px;
+        height: 2.25rem;
+        background-color: white;
+        position: absolute;
+        border-left: 1px solid #d0d4e4;
+        border-bottom: 1px solid #d0d4e4;
+        box-shadow: -1px 1px 1px rgb(0 0 0 / 10%);
+        border-bottom-left-radius: 4px;
+        left: 395px;
+    }
+    .collapseGroup .board-content-group-row-footer-corner{
+        display: none;
+    }
 </style>
