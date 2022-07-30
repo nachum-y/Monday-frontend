c9bcc860d44705593b7b67d371194f087cc55b79
Shiran Abir <shirkiabir@gmail.com>
2022-07-30 15:31:52 +0300

    Fixed search bar UI

--- src/components/board/board-header/board-header-filter.cmp.vue
+++ src/components/board/board-header/board-header-filter.cmp.vue
@@ -83,29 +83,6 @@
             </div>
           </div>
         </el-tooltip>
-        <el-tooltip placement="top" content="Hidden colunms" effect="dark">
-          <div class="board-filter-item-component columns-filter-component">
-            <div class="board-filter-item-content-wrapper item-state-exit-done">
-              <span class="">
-                <div
-                  class="monday-style-clickable board-filter-item-content columns-filter-component show-title-when-closed show-title-when-open monday-style-clickable--disable-text-selection">
-                  <i class="item-icon icon icon-hidden-eye-outline columns-filter-icon"></i>
-                  <span class="item-title">Hide</span>
-                </div>
-              </span>
-            </div>
-          </div>
-        </el-tooltip>
-        <div>
-          <button type="button"
-            class="menu-button--wrapper board-filters-overflow-menu-item menu-button--wrapper--size-32">
-            <svg viewBox="0 0 20 20" fill="currentColor" width="28" height="28" role="img" aria-hidden="true">
-              <path
-                d="M6 10.5C6 11.3284 5.32843 12 4.5 12 3.67157 12 3 11.3284 3 10.5 3 9.67157 3.67157 9 4.5 9 5.32843 9 6 9.67157 6 10.5zM11.8333 10.5C11.8333 11.3284 11.1618 12 10.3333 12 9.50492 12 8.83334 11.3284 8.83334 10.5 8.83334 9.67157 9.50492 9 10.3333 9 11.1618 9 11.8333 9.67157 11.8333 10.5zM17.6667 10.5C17.6667 11.3284 16.9951 12 16.1667 12 15.3383 12 14.6667 11.3284 14.6667 10.5 14.6667 9.67157 15.3383 9 16.1667 9 16.9951 9 17.6667 9.67157 17.6667 10.5z"
-                fill="currentColor"></path>
-            </svg>
-          </button>
-        </div>
       </div>
     </div>
   </div>
