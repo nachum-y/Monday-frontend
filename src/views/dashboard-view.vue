7aa82ffffa41f3642eda74cd2687e2bc47c2c4e1
Shiran Abir <shirkiabir@gmail.com>
2022-07-30 10:47:20 +0300

    Create dashboard chart - pie and  doughnut.

--- src/views/dashboard-view.vue
+++ src/views/dashboard-view.vue
@@ -3,14 +3,24 @@
         <div class="dashboard-item-holder">
             <bar-chart></bar-chart>
         </div>
+        <div class="dashboard-item-holder">
+            <pie-chart></pie-chart>
+        </div>
+        <div class="dashboard-item-holder">
+            <doughnut-chart></doughnut-chart>
+        </div>
     </div>
 </template>
 
 <script>
 import barChart from '../components/dashboard-cmps/bar-chart.cmp.vue'
+import pieChart from '../components/dashboard-cmps/pie-chart.cmp.vue'
+import doughnutChart from '../components/dashboard-cmps/doughnut-chart.cmp.vue'
 export default {
     components:{
         barChart,
+        pieChart,
+        doughnutChart,
     }
 }
 </script>
@@ -20,6 +30,7 @@ export default {
 .dashboard-content{
     padding: 1rem;
     background-color: #f6f7fb;
+    overflow-y: scroll;
 }
 
 .dashboard-item-holder{
@@ -27,6 +38,7 @@ export default {
     padding: 1rem;
     border-radius: 8px;
     background-color: #fff;
+    margin-bottom: 0.5rem;
 }
 
 </style>
