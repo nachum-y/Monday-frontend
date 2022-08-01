cdb5fa49197e3eca5fd029c93b25ed778fc8f7c8
Shiran Abir <shirkiabir@gmail.com>
2022-08-01 11:50:40 +0300

    Commit.

--- src/components/board/board-header/board-header.cmp.vue
+++ src/components/board/board-header/board-header.cmp.vue
@@ -34,7 +34,7 @@
     </div>
     <section class="sub-header-nav">
       <div class="view-btn-holder">
-        <button class="main-tab header-btn active-view">
+        <button class="main-tab header-btn" :class="isActive">
           <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="icon_component icon">
             <path
               d="M9.56992 2.1408C9.82591 1.95307 10.1741 1.95307 10.4301 2.1408L17.7028 7.47413C17.8896 7.61113 18 7.82894 18 8.06061V16.7879C18 17.1895 17.6744 17.5152 17.2727 17.5152H11.9394C11.5377 17.5152 11.2121 17.1895 11.2121 16.7879V13.1515H8.78788V16.7879C8.78788 17.1895 8.46227 17.5152 8.06061 17.5152H2.72727C2.32561 17.5152 2 17.1895 2 16.7879V8.06061C2 7.82894 2.11037 7.61113 2.29719 7.47413L9.56992 2.1408ZM3.45455 8.42914V16.0606H7.33333V12.4242C7.33333 12.0226 7.65894 11.697 8.06061 11.697H11.9394C12.3411 11.697 12.6667 12.0226 12.6667 12.4242V16.0606H16.5455V8.42914L10 3.62914L3.45455 8.42914Z">
@@ -44,22 +44,22 @@
           <span class="icon-v2-ellipsis"></span>
         </button>
         <div class="views-seprator"></div>
-        <button class="main-tab header-btn kanban-btn">
+        <button class="main-tab header-btn kanban-btn" :class="isActive">
           <router-link :to="{ name: 'kanban' }">kanban</router-link>
           <span class="icon-v2-ellipsis"></span>
         </button>
         <div class="views-seprator"></div>
-        <button class="main-tab header-btn dashboard-btn">
+        <button class="main-tab header-btn dashboard-btn" :class="isActive">
           <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
           <span class="icon-v2-ellipsis"></span>
         </button>
         <div class="views-seprator"></div>
-        <button class="main-tab header-btn map-btn">
+        <button class="main-tab header-btn map-btn" :class="isActive">
           <router-link :to="{ name: 'map'}">Map</router-link>
           <span class="icon-v2-ellipsis"></span>
         </button>
         <div class="views-seprator"></div>
-        <button class="main-tab header-btn card-btn">
+        <button class="main-tab header-btn card-btn" :class="isActive">
           <router-link :to="{ name: 'card' }">Cards</router-link>
           <span class="icon-v2-ellipsis"></span>
         </button>
@@ -82,6 +82,7 @@ export default {
       },
       openMenu: false,
       activeUser: null,
+      activeView: 'mainTable'
     }
   },
   created() {
@@ -118,8 +119,13 @@ export default {
       await this.$store.dispatch({ type: 'setActive', member })
       this.activeUser = this.$store.getters.getActiveUser
     },
+    isActive(){
+      return 'active-view'
+    }
+    
   },
   computed: {
+
 
   },
   components: {
