9ac8641c1c6af6c9ad516194c724e7a8923b8f8f
Meital Twito <tw.meital@gmail.com>
2022-08-01 15:28:55 +0300

    modal menu fix

--- src/components/modal.cmp.vue
+++ src/components/modal.cmp.vue
@@ -24,17 +24,5 @@ export default {
 }
 </script>
 <style>
-.modalCmp {
-    background-color: #fff;
-    width: fit-content;
-    position: absolute;
-    height: fit-content;
-    z-index: 9999;
-    padding: 1rem;
-    transition: all 50ms ease-in;
-    transition-property: opacity, width;
-    box-shadow: 0 4px 17px 6px rgb(0 0 0 / 10%);
-    border-radius: 0.5rem;
 
-}
 </style>
